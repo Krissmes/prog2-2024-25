fba9f1523db9c40c64bd6c105419a1f9be4e9c90
Krissmes <kriss.iesalnieks@gmail.com>
2024-09-10 12:30:24 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,33 @@
-print("lego")
+class Cilveks: 
+    def __init__ (self,vards,vecums,dzimums):
+        self.name=vards
+        self.age=vecums
+        self.sex=dzimums
+    def dzimene(self):
+        self.age +=1  
+    def MainitVardu(self):
+        if self.name == "Krišs":
+            vards = "Pritkovs"
+        else:
+            vards = self.name
+
+    def BazarsPaSevi(self):
+        if self.sex == "v":
+            dzimums = "vīrietis"
+        elif self.sex == "s":
+            dzimums = "sieviete"
+        else:
+            dzimums = self.sex
+
+        print("Sveiki, mani sauc {} man ir {} gadi un es esmu {}".format(self.name, self.age, dzimums ) )
+
+
+persona = Cilveks("Krišs", 18, "v")
+persona1 = Cilveks("Rada", 16, "s")
+
+
+
+persona.dzimene()
+
+persona.BazarsPaSevi()
+
